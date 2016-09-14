4ce938c9cfc700f07dd10004b51963d7e560c262
sano-yutaka-gn <sano_yutaka_gn@cyberagent.co.jp>
2016-09-14 19:51:52 +0900

    eslintの設定  	deleted:    .bowerrc  	modified:   .eslintrc  	modified:   gulpfile.js/config.js  	modified:   package.json

--- gulpfile.js/config.js
+++ gulpfile.js/config.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var pkg  = require('../package.json');
+var pkg = require('../package.json');
 
 module.exports = {
-  name  : pkg.name,
-  dirs  : {
+  name : pkg.name,
+  dirs : {
     src   : 'src',
     dest  : 'public',
     build : 'build'
