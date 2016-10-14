a4adeb75fc2da21105a4388526cb62b86e45c512
verytired <sano.yutaka@gmail.com>
2016-10-14 12:36:21 +0900

    fixed error  	modified:   build/js/app.js  	modified:   public/index.html  	modified:   src/js/ShaderTest.js

--- src/js/ShaderTest.js
+++ src/js/ShaderTest.js
@@ -8,23 +8,42 @@ class ShaderTest {
     let container = document.getElementById('container');
     container.appendChild(renderer.view);
 
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
     let stage = new PIXI.Container();
+
+    // smoke shader
+    let uniforms = {};
+    uniforms.resolution = { type : 'v2', value : { x : width, y : height}};
+    uniforms.alpha = { type : '1f', value : 1.0};
+    uniforms.shift = { type : '1f', value : 1.6};
+    uniforms.time = {type : '1f', value : 0};
+    uniforms.speed = {type : 'v2', value : {x : 0.7, y : 0.4}};
+
+    let shaderCode = document.getElementById('fragShader').innerHTML;
+    let smokeShader = new PIXI.Filter(null, shaderCode, uniforms);
+
+    let bg = PIXI.Sprite.fromImage('http://www.goodboydigital.com/pixijs/pixi_v3_github-pad.png');
+    bg.width = width;
+    bg.height = height;
+    bg.filters = [smokeShader];
+    stage.addChild(bg);
+
     let logo = PIXI.Sprite.fromImage('http://www.goodboydigital.com/pixijs/pixi_v3_github-pad.png');
-    logo.x = window.innerWidth / 2;
-    logo.y = window.innerHeight / 2;
+    logo.x = width / 2;
+    logo.y = height / 2;
     logo.anchor.set(0.5);
+    logo.blendMode = PIXI.BLEND_MODES.ADD;
     stage.addChild(logo);
 
-    let shaderCode = document.getElementById('shader').innerHTML;
-    // Create our Pixi filter using our custom shader code
-    let simpleShader = new PIXI.AbstractFilter('', shaderCode);
-    // Apply it to our object
-    logo.filters = [simpleShader];
-
+    let count = 0;
     function animate() {
       // start the timer for the next animation loop
       requestAnimationFrame(animate);
       // this is the main render call that makes pixi draw your container and its children.
+      count+=0.01;
+      smokeShader.uniforms.time = count;
       renderer.render(stage);
     }
     animate();
